449127d5f89f6b04e23b155964c78974b5c4c4bc
trapacska <trapacska@gmail.com>
2018-03-01 16:20:43 +0100

    update

--- main.go
+++ main.go
@@ -1,31 +1,164 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"net/url"
 	"os"
-	"os/exec"
+	"path/filepath"
+	"strings"
+	"time"
+
+	"github.com/bitrise-io/go-utils/log"
+	"github.com/bitrise-io/go-utils/pathutil"
+	"github.com/bitrise-tools/go-steputils/tools"
 )
 
-func main() {
-	fmt.Println("This is the value specified for the input 'example_step_input':", os.Getenv("example_step_input"))
+const envKey = "GENERIC_FILE_STORAGE"
 
-	//
-	// --- Step Outputs: Export Environment Variables for other Steps:
-	// You can export Environment Variables for other Steps with
-	//  envman, which is automatically installed by `bitrise setup`.
-	// A very simple example:
-	cmdLog, err := exec.Command("bitrise", "envman", "add", "--key", "EXAMPLE_STEP_OUTPUT", "--value", "the value you want to share").CombinedOutput()
-	if err != nil {
-		fmt.Printf("Failed to expose output with envman, error: %#v | output: %s", err, cmdLog)
-		os.Exit(1)
+type file struct {
+	Name string
+	URL  string
+}
+
+type files []file
+
+func (fs files) String() string {
+	fileNames := []string{}
+	for _, file := range fs {
+		fileNames = append(fileNames, file.Name)
 	}
-	// You can find more usage examples on envman's GitHub page
-	//  at: https://github.com/bitrise-io/envman
+	return strings.Join(fileNames, "\n  ")
+}
 
-	//
-	// --- Exit codes:
-	// The exit code of your Step is very important. If you return
-	//  with a 0 exit code `bitrise` will register your Step as "successful".
-	// Any non zero exit code will be registered as "failed" by `bitrise`.
-	os.Exit(0)
+func isGenericKey(key string) bool {
+	return strings.HasPrefix(key, "BITRISEIO_") && strings.HasSuffix(key, "_URL")
+}
+
+func getStorageTempDirPath() (string, error) {
+	return pathutil.NormalizedOSTempDirPath("_GENERIC_FILE_STORAGE_")
+}
+
+func splitEnv(env string) (string, string) {
+	e := strings.Split(env, "=")
+	return e[0], strings.Join(e[1:], "=")
+}
+
+func urlToFile(urlStr string) (file, error) {
+	url, err := url.Parse(urlStr)
+	if err != nil {
+		return file{}, err
+	}
+	return file{URL: urlStr, Name: filepath.Base(url.Path)}, nil
+}
+
+func getFiles() ([]file, error) {
+	files := []file{}
+
+	for _, env := range os.Environ() {
+		key, value := splitEnv(env)
+
+		if !isGenericKey(key) {
+			continue
+		}
+
+		f, err := urlToFile(value)
+		if err != nil {
+			return nil, err
+		}
+
+		files = append(files, f)
+	}
+
+	return files, nil
+}
+
+func downloadFile(filepath string, url string) error {
+	// Create the file
+	out, err := os.Create(filepath)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	// Get the data
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+
+	if resp.StatusCode < 200 || resp.StatusCode > 299 {
+		return fmt.Errorf("non successful statusCode: %d\nurl: %s\nbody:\n%s", resp.StatusCode, url, string(body))
+	}
+
+	// Write the body to file
+	_, err = io.Copy(out, bytes.NewReader(body))
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func downloadFiles(path string, files []file) error {
+	for _, file := range files {
+		if err := downloadFile(filepath.Join(path, file.Name), file.URL); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func failf(f string, args ...interface{}) {
+	log.Errorf(f, args...)
+	os.Exit(1)
+}
+
+func main() {
+	log.Infof("Create Storage dir:")
+
+	storageDir, err := getStorageTempDirPath()
+	if err != nil {
+		failf("Failed to create storage temp dir, error: %s", err)
+	}
+
+	if err := tools.ExportEnvironmentWithEnvman(envKey, storageDir); err != nil {
+		failf("Failed to export path: %s to the env: %s, error: %s", storageDir, envKey, err)
+	}
+
+	log.Printf("  %s: %s", envKey, storageDir)
+	log.Donef("- Done")
+
+	fmt.Println()
+
+	log.Infof("Parsing Generic Storage Files:")
+
+	fs, err := getFiles()
+	if err != nil {
+		failf("Failed to fetch file list, error: %s", err)
+	}
+
+	log.Printf("  %s", files(fs))
+	log.Donef("- Done")
+
+	fmt.Println()
+
+	log.Infof("Downloading %d files:", len(fs))
+	started := time.Now()
+	err = downloadFiles(storageDir, fs)
+	if err != nil {
+		failf("Failed to download files, error: %s", err)
+	}
+	log.Printf("  Took: %s", time.Since(started))
+
+	log.Donef("- Done")
 }
