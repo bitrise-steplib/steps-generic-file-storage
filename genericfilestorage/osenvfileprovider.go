90872ec8e32248f5bbc4ce695e86c4db398ff269
Daniel Serleg <daniel.serleg@bitrise.io>
2020-12-02 16:28:27 +0100

    Fixes an issue with the error log handling (#10)
    * Log keys, unwrap error, avoid unnecessary checks
    
    * Print only key
    
    * More detailed test
    
    * Sample secret file
    
    * Remove the debug lines
    
    Co-authored-by: Zsolt Marta <zsolt.marta@bitrise.io>

--- genericfilestorage/osenvfileprovider.go
+++ genericfilestorage/osenvfileprovider.go
@@ -65,27 +65,28 @@ func (provider OsEnvFileProvider) isIgnoredKey(key string) bool {
 
 func (provider OsEnvFileProvider) shouldHandle(key, url string) bool {
 	validKey := provider.isGenericKey(key) && !provider.isIgnoredKey(key)
-	validURL := provider.isValidURL(url)
-
-	return validKey && validURL
+	if !validKey {
+		return false
+	}
+	return provider.isValidURL(key, url)
 }
 
-func (provider OsEnvFileProvider) isValidURL(rawURL string) bool {
+func (provider OsEnvFileProvider) isValidURL(key, rawURL string) bool {
 	parsedURL, err := url.ParseRequestURI(rawURL)
 	if err != nil {
-		log.Printf("Failed to parse URL: %s. %s", rawURL, err)
+		log.Printf("Failed to parse URL: %s. %s", key, err.(*url.Error).Unwrap())
 		return false
 	}
 
 	containsHost := strings.Contains(parsedURL.Host, awsHostPart)
 	if !containsHost {
-		log.Warnf("URL: %s missing required host part.", rawURL)
+		log.Warnf("URL: %s missing required host part.", key)
 		return false
 	}
 
 	conatinsPath := strings.Contains(parsedURL.Path, awsPathPart)
 	if !conatinsPath {
-		log.Warnf("URL: %s missing required path part.", rawURL)
+		log.Warnf("URL: %s missing required path part.", key)
 		return false
 	}
 
